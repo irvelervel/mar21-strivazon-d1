9bb83c792611b2d7a9b126e675423b825536d121
Stefano Casasola <ste.casasola@gmail.com>
2021-06-24 11:48:43 +0200

    Last break

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,37 @@
 // here I write my action creators
 // functions that return actions
 
+// redux-thunk is a middleware allowing you to dispatch not only JS OBJECTS anymore,
+// but also gives redux the ability to dispatch FUNCTIONS (can be async)
+
+// our goal is to write an action creator that does not only return the JS OBJECT (the action)
+// but also handling the async fetch, getting the data out of the API and then dispatch
+// the action with the array of books as the payload
+
+export const getBooksAction = () => {
+  return async (dispatch, getState) => {
+    // dispatch here is coming thanks to redux-thunk
+    // fetch the books here!
+    // this function can handle async operations (like a fetch) or conditionally dispatch the action,
+    // delay the dispatching of an action, do complex data manipulation
+    try {
+      let resp = await fetch('https://striveschool-api.herokuapp.com/food-books')
+      console.log(getState())
+      if (resp.ok) {
+        let books = await resp.json()
+        dispatch({
+          type: 'GET_BOOKS',
+          payload: books,
+        })
+      } else {
+        console.log('error')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
 export const addItemToCartAction = (book) => {
   return {
     type: 'ADD_ITEM_TO_CART',
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 // what the initial state of the application will be?
 
-import { createStore } from 'redux'
-import mainReducer from '../reducers'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import bookReducer from '../reducers/book'
+import cartReducer from '../reducers/cart'
+import userReducer from '../reducers/user'
+
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const initialState = {
   cart: {
@@ -10,9 +16,20 @@ export const initialState = {
   user: {
     firstName: '',
   },
+  books: {
+    stock: [],
+  },
 }
 
-const configureStore = () =>
-  createStore(mainReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const bigReducer = combineReducers({
+  cart: cartReducer,
+  user: userReducer,
+  books: bookReducer,
+})
+
+// splitting up the reducers will make them independent and not-aware any more of the big picture
+// the state argument every reducer is now receiving is just their slice of the cake
+
+const configureStore = () => createStore(bigReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
 
 export default configureStore
